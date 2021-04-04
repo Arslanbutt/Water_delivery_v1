f4eff6c2e01d1e8d8fee5f05c87128bf217ebf57
Arslan <arslanbutt7545@gmail.com>
2021-04-04 11:17:19 +0500

    Add main page layout, image not finalized some other changes maybe done later

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'constants.dart';
+import 'components/rounded_button.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key, this.title}) : super(key: key);
@@ -15,9 +17,57 @@ class _HomePageState extends State<HomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [],
+        child: Padding(
+          padding: EdgeInsets.all(24.0),
+          child: Column(
+            //mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Flexible(
+                child: Hero(
+                  tag: 'Logo',
+                  child: Container(
+                    height: 200,
+                    child: Image.asset('images/42363.jpg'),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 48.0,
+              ),
+              Text(
+                'Delivered to your door',
+                style: TextStyle(color: Colors.lightBlueAccent, fontSize: 20.0),
+              ),
+              SizedBox(
+                height: 48.0,
+              ),
+              TextField(
+                style: TextStyle(color: Colors.black45),
+                textAlign: TextAlign.center,
+                keyboardType: TextInputType.phone,
+                decoration: kInputBoxDecoration.copyWith(
+                    hintText: 'Enter Your Phone Number'),
+              ),
+              SizedBox(
+                height: 48.0,
+              ),
+              TextField(
+                style: TextStyle(color: Colors.black45),
+                textAlign: TextAlign.center,
+                keyboardType: TextInputType.name,
+                decoration:
+                    kInputBoxDecoration.copyWith(hintText: 'Enter your name'),
+              ),
+              SizedBox(
+                height: 48.0,
+              ),
+              RoundedButton(
+                onPressed: () {},
+                buttonText: 'Proceed',
+              )
+            ],
+          ),
         ),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
